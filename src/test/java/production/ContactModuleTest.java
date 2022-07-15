0d236803afbd425369979b02a8518dd69200ddbf
naveen magnur <naveen magnur@magnur>
2022-07-15 16:57:35 +0530

    Add the printing statement in framework

--- src/test/java/production/ContactModuleTest.java
+++ src/test/java/production/ContactModuleTest.java
@@ -6,9 +6,11 @@ public class ContactModuleTest {
 @Test
 public void createContactTest() {
 	System.out.println("createContactTest executed");
+	System.out.println("contact module modifyed");
 }
 @Test
 public void createOrganizationTest() {
 	System.out.println("createOrganizationTest executed");
+	System.out.println("organization module modifyed");
 }
 }
--- src/test/java/production/OrganizationModuleTest.java
+++ src/test/java/production/OrganizationModuleTest.java
@@ -6,10 +6,12 @@ public class OrganizationModuleTest {
 	@Test
 	public void createOrgTest() {
 		System.out.println("createOrgTest executed");
+		System.out.println("createOrgTest is not executed ");
 	}
 	@Test
 	public void createOrganizationAndDeleteTest() {
 		System.out.println("createOrganizationAndDeleteTest executed");
+		System.out.println("createOrganizationAndDeleteTest is not executed");
 	}
 		}
 
